dd9570a0f7ceea2a04b1566d1262960ec65f20ed
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2013-10-20 01:47:38 +0300

    add assemblyInfo.fs

---
+++ UnionArgParser/assemblyInfo.fs
@@ -0,0 +1,9 @@
+﻿namespace UnionArgParser
+
+    open System.Reflection
+
+#if BUILD_STRONG_NAME
+    [<assembly:AssemblyKeyFile("../../Lib/key.snk")>]
+#endif
+    [<assembly:AssemblyVersion("0.5.4.*")>]
+    do()
