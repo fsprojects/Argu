854679cf39d771fa49afa67ac1caf03494795dac
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-06-03 00:48:20 +0300

    update version

--- UnionArgParser/assemblyInfo.fs
+++ UnionArgParser/assemblyInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.6.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.6.2")>]
+[<assembly: AssemblyVersionAttribute("0.6.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.6.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.6.2"
+    let [<Literal>] Version = "0.6.4"
