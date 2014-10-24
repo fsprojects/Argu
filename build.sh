af22676eaf599a3754c17edf74d891b93dfa9acb
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-10-24 22:36:11 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+if [ "X$OS" = "XWindows_NT" ] ; then
+  # use .Net
 
-if [ ! -f packages/FAKE/tools/Fake.exe ]; then
-  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+    .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  fi
+
+  packages/FAKE/tools/FAKE.exe build.fsx $@
+
+else
+
+  # use mono
+  if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+    mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  fi
+
+  mono packages/FAKE/tools/FAKE.exe build.fsx $@
 fi
-
-mono packages/FAKE/tools/FAKE.exe build.fsx $@
