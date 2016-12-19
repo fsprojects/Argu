96d61e1f5253c5c025d5bafa720a5fc8ed7b2110
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2016-12-19 16:22:57 +0000

    clean ups

--- tests/Argu.Tests/tests.fsx
+++ tests/Argu.Tests/tests.fsx
@@ -69,7 +69,7 @@ with
             | Options _ -> "enumeration of options."
             | Silent -> "just be silent."
 
-let parser = ArgumentParser.Create<GitArgs>(programName = "gadget", helpTextMessage = "Gadget -- my awesome CLI tool", errorHandler = ProcessExiter(fun _ -> ConsoleColor.))
+let parser = ArgumentParser.Create<GitArgs>(programName = "gadget", helpTextMessage = "Gadget -- my awesome CLI tool")
 
 parser.PrintCommandLineArgumentsFlat [Options(Some MyEnum.First) ; Push(toParseResults [Remote ("origin", "master")])]
 
@@ -86,11 +86,3 @@ cparser.PrintUsage() |> Console.WriteLine
 parser.PrintCommandLineSyntax(usageStringCharacterWidth = 10) |> Console.WriteLine
 parser.PrintCommandLineSyntax(usageStringCharacterWidth = 100) |> Console.WriteLine
 cparser.PrintCommandLineSyntax() |> Console.WriteLine
-
-open System
-open System.Text
-
-
-let text = "Sed \nut perspiciatis, unde\n omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?"
-
-wordwrap text 60 "\n"
