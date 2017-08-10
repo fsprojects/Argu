2c6ec9a1d5f1b5ed050e93facb56cf6fef683e0a
Lukas Rieger <lr@live.de>
2017-08-10 03:37:04 +0200

    wip

--- build.fsx
+++ build.fsx
@@ -171,7 +171,7 @@ Target "ReleaseGitHub" (fun _ ->
 
 let dotnetcliVersion = "2.1.0-preview1-007002"
 let dotnetSDKPath = System.Environment.GetFolderPath Environment.SpecialFolder.LocalApplicationData </> "dotnetcore" </> dotnetcliVersion |> FullName
-let dotnetExePath = dotnetSDKPath </> (if isWindows then "dotnet.exe" else "dotnet") |> FullName
+let mutable dotnetExePath = dotnetSDKPath </> (if isWindows then "dotnet.exe" else "dotnet") |> FullName
 
 // .NET Core SDK and .NET Core
 
@@ -187,51 +187,7 @@ let dotnet workDir = runCmdIn workDir "dotnet"
 
 
 Target "InstallDotNetCore" (fun _ ->
-    let correctVersionInstalled = 
-        try if FileInfo(dotnetExePath |> FullName).Exists then
-                let processResult = 
-                    ExecProcessAndReturnMessages (fun info ->  
-                    info.FileName <- dotnetExePath
-                    info.WorkingDirectory <- __SOURCE_DIRECTORY__
-                    info.Arguments <- "--version") (TimeSpan.FromMinutes 30.)
-                processResult.Messages |> separated "" = dotnetcliVersion
-            else false
-        with  _ -> false
-
-    if correctVersionInstalled then
-        tracefn "dotnetcli %s already installed" dotnetcliVersion
-    else
-        CleanDir dotnetSDKPath
-        let archiveFileName = 
-            if isLinux then sprintf "dotnet-dev-ubuntu-x64.%s.tar.gz" dotnetcliVersion
-            elif Fake.EnvironmentHelper.isMacOS then sprintf "dotnet-dev-osx-x64.%s.tar.gz" dotnetcliVersion
-            else sprintf "dotnet-dev-win-x64.%s.zip" dotnetcliVersion
-        let downloadPath = sprintf "https://dotnetcli.azureedge.net/dotnet/Sdk/%s/%s" dotnetcliVersion archiveFileName
-        let localPath = Path.Combine(dotnetSDKPath, archiveFileName)
-
-        tracefn "Installing '%s' to '%s'" downloadPath localPath
-        
-        use webclient = new Net.WebClient ()
-        webclient.DownloadFile (downloadPath, localPath)
-
-        if isLinux || isMacOS then
-            let assertExitCodeZero x =
-                if x = 0 then () else
-                failwithf "Command failed with exit code %i" x
-
-            Shell.Exec ("tar", sprintf """-xvf "%s" -C "%s" """ localPath dotnetSDKPath)
-            |> assertExitCodeZero
-        else  
-            System.IO.Compression.ZipFile.ExtractToDirectory(localPath, dotnetSDKPath)
-        
-        tracefn "dotnet cli path - %s" dotnetSDKPath
-        System.IO.Directory.EnumerateFiles dotnetSDKPath
-        |> Seq.iter (fun path -> tracefn " - %s" path)
-        System.IO.Directory.EnumerateDirectories dotnetSDKPath
-        |> Seq.iter (fun path -> tracefn " - %s%c" path System.IO.Path.DirectorySeparatorChar)
-
-    let oldPath = System.Environment.GetEnvironmentVariable "PATH"
-    System.Environment.SetEnvironmentVariable("PATH", sprintf "%s%s%s" dotnetSDKPath (System.IO.Path.PathSeparator.ToString()) oldPath)
+    dotnetExePath <- DotNetCli.InstallDotNetSDK dotnetcliVersion
 )
 
 let netcoreSrcFiles =  [ __SOURCE_DIRECTORY__  </> "src/Argu.netcore/Argu.netcore.fsproj"]
