4c353d9871d318d2459b518ca3dc56cb8c89b112
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2018-01-30 02:00:01 +0000

    fix output dir configuration

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -4,7 +4,7 @@
 // --------------------------------------------------------------------------------------
 
 // Binaries that have XML documentation (in a corresponding generated XML file)
-let referenceBinaries = [ "net40/Argu.dll" ]
+let referenceBinaries = [ "net45/Argu.dll" ]
 // Web site location for the generated documentation
 let website = "/Argu"
 
