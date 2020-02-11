86587c2a026e0b29b0f1d118fcfa1c533b518b1d
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-02-11 01:24:36 +0000

    restore using paket instead of fake

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 @echo off
 
 dotnet tool restore
+dotnet paket restore
 dotnet fake run build.fsx %*
--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -eu
 cd `dirname $0`
 
 dotnet tool restore
+dotnet paket restore
 dotnet fake run build.fsx "$@"
