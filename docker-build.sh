6773a1c69584888b86baa3bd76cc193cef748d6a
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-03-12 16:40:40 +0000

    update docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -5,11 +5,15 @@ cd `dirname $0`
 IMAGE_LABEL="argu-build"
 
 # docker build
-docker build -t $IMAGE_LABEL .
+docker build \
+    --build-arg "GIT_USER_NAME=$(git config user.name)" \
+    --build-arg "GIT_USER_EMAIL=$(git config user.email)" \
+    -t $IMAGE_LABEL \
+    .
 
 # dotnet build, test & nuget publish
 docker run -t --rm \
-           -e GITHUB_TOKEN=$GITHUB_TOKEN \
-           -e NUGET_KEY=$NUGET_KEY \
-           $IMAGE_LABEL \
-           ./build.sh "$@"
+    -e GITHUB_TOKEN=$GITHUB_TOKEN \
+    -e NUGET_KEY=$NUGET_KEY \
+    $IMAGE_LABEL \
+    ./build.sh "$@"
